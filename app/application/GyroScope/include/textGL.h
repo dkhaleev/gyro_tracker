2af7be3c60b83d3f17e506422f913757ccf0417a
Dmitry Khaleev <dkhaleev@gmail.com>
2021-03-11 22:33:31 +0200

    First Phase: App: TextGL skeleton

--- app/application/GyroScope/include/textGL.h
+++ app/application/GyroScope/include/textGL.h
@@ -10,9 +10,24 @@ class TextGL {
     ~TextGL();
 
     void setFont(QFont &font);
+    void setString(QString textString);
+    void setPlane(QString plane="xy");
+    void setZeroPoint(float x=0, float y=0, float z=0);
+    void setHeight(float height=1);
+    void setThickness(float thickness=1);
+    void setColour(QColor &fontColor);
+    void renderText();
 
   private:
-  QFont* font;
+    QFont* font;
+    QString* textString;
+    QString* plane;
+    float* zeroX;
+    float* zeroY;
+    float* zeroZ;
+    float* height;
+    float* thickness;
+    QColor* fontColor;
 
 };
 
--- app/application/GyroScope/src/textGL.cpp
+++ app/application/GyroScope/src/textGL.cpp
@@ -5,8 +5,45 @@ TextGL::TextGL(){
 }
 
 void TextGL::setFont(QFont &font){
-  QTextStream(stdout) << "Set font \r\n";
+  QTextStream(stdout) << "Set font" << "\r\n";
   this->font = &font;
+}
+
+void TextGL::setString(QString textString=""){
+  QTextStream(stdout) << "Set String " << textString << "\r\n";
+  this->textString = &textString;
+}
+
+
+void TextGL::setPlane(QString plane){
+  QTextStream(stdout) << "Set plane " << plane << "\r\n";
+  this->plane = &plane;
+}
+
+void TextGL::setZeroPoint(float x, float y, float z){
+  QTextStream(stdout) << "Zero point: X:" << x << " Y: " << y << " Z:" << z << "\r\n";
+  this->zeroX = &x;
+  this->zeroY = &y;
+  this->zeroZ = &z;
+}
+
+void TextGL::setHeight(float height){
+  QTextStream(stdout) << "Set height: " << height << "\r\n";
+  this->height = &height;
+}
+
+void TextGL::setThickness(float thickness){
+  QTextStream(stdout) << "Set thickness: " << thickness << "\r\n";
+  this->thickness = &thickness;
+}
+
+void TextGL::setColour(QColor &fontColor){
+  QTextStream(stdout) << "Set colour " << "\r\n";
+  this->fontColor = &fontColor;
+}
+
+void TextGL::renderText(){
+  QTextStream(stdout) << "Render Text \r\n";
 }
 
 TextGL::~TextGL(){}
